8e8eaab787501f3faeeff08b363b01bf5a85e615
Daniel Littlewood <danielittlewood@gmail.com>
2019-04-14 19:45:34 +0100

    reads commands and spits them back at you

--- cfg_cli.rb
+++ cfg_cli.rb
@@ -1,3 +1,13 @@
-ARGV.each do |input_line| 
-  puts input_line
+begin 
+  config_file = File.readlines(ARGV[0]).each do |line|
+    next if line =~ /\A(\s)*\z/ # skips blank lines
+    cmd,args = line.split(' : ')
+    raise "No command detected!" if cmd.nil?
+    raise "No arguments detected!" if args.nil?
+    puts cmd
+    puts args
+  end
+rescue => e
+  puts "ERROR!"
+  puts e.message
 end
